ef9afaad13fb7c5686a53027f690a65ee2b849fc
Sam A Miller <62508068+SamuelAMiller1@users.noreply.github.com>
2021-03-10 11:55:43 -0500

    Update rna_seq_pipeline.sh

--- rna_seq_pipeline.sh
+++ rna_seq_pipeline.sh
@@ -1,114 +1,104 @@
 #!/bin/bash
 
-# Activate enviornment
+while [[ $# -gt 0 ]]; do
+  key=$1
+  case $key in
+  
+    -s|--seq-dir)
+      seqdir="$2"
+      shift 2
+      ;;
+    -a|--assembly)
+      ASSEMBLY="$2"
+      shift 2
+      ;;
+    -o|--out-dir)
+      outdir="$2"
+      shift 2
+      ;;
+    -t|--threads)
+      CORES="$2"
+      shift 2
+      ;;
 
-export PATH=/N/u/millesaa/Carbonate/miniconda3/envs/rnaseq/bin:$PATH
+  esac
+done
+
 
 # Navigate to parent directory
-
-cd /N/slate/millesaa/dll1_corin_ht29_rnaseq/dll1_corin_rnaseq
+cd $outdir
 
 # Collect fastqs into array
-FASTQS=($(find ./sequences -type f -name "*.fastq.gz"))
-
-# Run FASTQ
-
-fastq \
- -t 6 \
- ${FASTQS[@]}
+FASTQS=($(find $seqdir -type f -name "*.fastq.gz"))
 
 # Make results file
 
 mkdir -p results/fastqc/
 
-# Move .html files
+# Run FASTQC
 
-HTML=($(find ./sequences -type f -name "*fastqc.html"))
+fastqc \
+ -t $CORES \
+ -o results/fastqc \
+ ${FASTQS[@]}
 
-mv ${HTML[@]} results/fastqc/
+# Remove .zip files (if unnecessary to keep)
 
-# Remove .zip files
-
-ZIP=($(find ./sequences -type f -name "*fastqc.zip"))
+ZIP=($(find results/fastqc -type f -name "*fastqc.zip"))
 
 for files in "${ZIP[@]}"; do
  rm -f "$files"
 done
 
 
-_______________________
-
-
-
-
-# Assign variables to annotation and assembly file locations
-
-ASSEMBLY='ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_37/gencode.v37.transcripts.fa.gz'
-
-ANNOTATION='ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_37/gencode.v37.annotation.gtf.gz'
-
-# Navigate to parent directory
-
-cd /N/slate/millesaa/dll1_corin_ht29_rnaseq/dll1_corin_rnaseq
+#########################
+# INDEXING AND COUNTING #
+#########################
 
 #Create directories to store the index and quant matrix
 
-mkdir -p genome
+mkdir -p genome_index
 mkdir -p quants
 
 ##################################
 ## Generate Salmon Genome Index ##
 ##################################
 
-# Download and unpack the genome assembly and annotation
-
-curl $ASSEMBLY -o ./genome/gencode.v37.transcripts.fa.gz
-
 # Index the Genome
 
-salmon index -t ./genome/gencode.v37.transcripts.fa.gz -i ./genome/gencode_v37_index --gencode
-
+salmon index -t $ASSEMBLY -i ./genome_index
 
 ##################################
 ## Generate Salmon Pseudocounts ##
 ##################################
 
 # Quantify reads
-# Next run, add flags --seqBias --useVBOpt
 
-for fn in sequences/ILMN_906_Ohagan_{1..24};
- do
- samp=`basename ${fn}`
- salmon quant -i ./genome/gencode_v37_index -l A \
-        -1 ${fn}/*\_R1_001.fastq.gz \
-        -2 ${fn}/*\_R2_001.fastq.gz \
+# Create array for FASTQ files
+
+FASTQS=($(find $seqdir -name "*\.fastq"))
+
+#Subset FASTQ's in individual arrays for Read 1 and Read 2 
+
+FASTQSONE=(`echo ${FASTQS[@]} | sed 's/ /\n/g' | grep _R1_`)
+FASTQSTWO=(`echo ${FASTQS[@]} | sed 's/ /\n/g' | grep _R2_`)
+
+
+# Align the reads with STAR
+
+for n in {0..${#FASTQSONE[@]}}; do
+
+ R1=${FASTQSONE[${n}]}
+ R2=${FASTQSTWO[${n}]}
+
+ samp=`basename ${R1}`
+ salmon quant -i ./genome_index -l A \
+        -1 $R1 \
+        -2 $R2 \
 	-o quants/${samp} \
 	--validateMappings \
 	--seqBias
 done
 
-
-________________________________
-
-
-#!/bin/bash
-
-## Create a single .tsv file that contains gencode v37 transcript and gene ID's
-
-# Copy transcript ID's to text file
-
-zgrep -P -o 'ENST\d{11}' genome/gencode.v37.transcripts.fa.gz > enst.txt
-
-
-# Copy gene ID's to text file
-
-zgrep -P -o 'ENSG\d{11}' genome/gencode.v37.transcripts.fa.gz > ensg.txt
-
-# Conjoin txt files into one tsv file
-
-paste enst.txt ensg.txt > gene_map.tsv
-
-
-________________________________________________
 
 
